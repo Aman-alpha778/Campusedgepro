e0969387f87713876382efd24dc821186d093cbb
Aman sharma <ajsharma89096@gmail.com>
2026-05-30 12:33:51 +0530

    updated the documentation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,7 @@
       <a href="features.html">Product Features</a>
       <a href="pricing.html">Pricing</a>
       <a href="blog.html">Blogs</a>
-      <a href="docs/index.html">Documentation</a>
+      <a href="documentation.html">Documentation</a>
       <a href="contact.html">Contact Us</a>
       <a class="button" href="demo.html">Book Demo</a>
     </div>
